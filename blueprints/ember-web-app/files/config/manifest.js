80b2f631edd3c5a62859b4df1da087e94ee13da5
Santiago Ferreira <san650@gmail.com>
2016-12-06 21:01:19 -0300

    Add link to documentation in blueprint

--- blueprints/ember-web-app/files/config/manifest.js
+++ blueprints/ember-web-app/files/config/manifest.js
@@ -2,6 +2,9 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
   return {
     name: "<%= name %>",
     short_name: "<%= name %>",
